e73173969d2e2d307d7185317b1e0efe6e7b60e6
Supriya Sanjay Gaikwad <138199157+Supriya90921@users.noreply.github.com>
2025-12-29 19:35:39 +0530

    Rename Greetings component to Greeting

--- src/Greeting.js
+++ src/Greeting.js
@@ -1,6 +1,6 @@
-function Greetings(props) //we cant use  {} for props cause it itself indicates props so if we use it here means we are calling props to call props
+function Greeting(props) //we cant use  {} for props cause it itself indicates props so if we use it here means we are calling props to call props
 {
   return <h2>Welcome, {props.name} 🌸</h2>;
 }
 
-export default Greetings;
+export default Greeting;
