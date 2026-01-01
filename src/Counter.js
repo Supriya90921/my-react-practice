07f2492bdd7a470dc3bea5ee5c0cf3af92c80ad0
Supriya Sanjay Gaikwad <138199157+Supriya90921@users.noreply.github.com>
2026-01-01 20:33:18 +0530

    Update Counter.js

--- src/Counter.js
+++ src/Counter.js
@@ -1,1 +1,25 @@
+import {useState ,UseEffect}from react;
 
+function Counter()
+{
+  const[count ,setcount]=useState(0);
+
+  useEffect=(()=>
+    {
+      console.log("Component mounted or updated");
+      return
+      (
+        console.log("COmponent unmounted");
+      )
+    }[];
+
+return(
+     <div><h1> You have counted {count}
+     <button onCLick={setcount(count+1)}>Click me </button>
+</div>
+     
+  
+)
+  
+}
+export default Counter;
