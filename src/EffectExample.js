37f8518efeb63ce9aa077ad7a6a5756947dd3479
Supriya Sanjay Gaikwad <138199157+Supriya90921@users.noreply.github.com>
2026-01-02 23:42:22 +0530

    Fix component unmount and button click handler

--- src/EffectExample.js
+++ src/EffectExample.js
@@ -9,12 +9,12 @@ function EffectExample()
       console.log("component mounted and updated")
       return(
         console.log("component unmount")
-     )[load];
-    });
+     )
+    },[load]);
     
             return(
                   <div> <h1>Hii this is load {load}</h1>
-                <button onClick={showload(() => load + 1)}> click me </button>
+                <button onClick={()=> showload(load + 1)}> click me </button>
              </div>
             );
 }
