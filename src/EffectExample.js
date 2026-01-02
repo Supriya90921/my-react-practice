ce2e07584efb8fcf7a1b1342ef220c2b30d11fa8
Supriya Sanjay Gaikwad <138199157+Supriya90921@users.noreply.github.com>
2026-01-02 23:22:43 +0530

    Correct EffectExample component functionality
    Fix syntax errors and improve useEffect implementation.

--- src/EffectExample.js
+++ src/EffectExample.js
@@ -1,1 +1,19 @@
+function EffectExaple()
+{
+  const[load, showload]=useState(0);
 
+  useEffect(()=>
+    {
+      console.log("component mounted and updated")
+      return
+      {
+        console.log("component unmount")
+      }[load]);
+    }
+            return(
+                  <div> <h1>Hii this is load {load}</h1>
+                <button onClick=showload()=>{load+1}> click me </button>
+             
+            );
+}
+export default EffectExample;
