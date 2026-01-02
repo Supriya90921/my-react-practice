0f324b7237e8b4ca94e71f69078015c08f2cbf82
Supriya Sanjay Gaikwad <138199157+Supriya90921@users.noreply.github.com>
2026-01-02 23:34:49 +0530

    Correct EffectExample component and useEffect syntax
    Fix component naming and syntax errors in EffectExample.js

--- src/EffectExample.js
+++ src/EffectExample.js
@@ -1,21 +1,21 @@
-import {useState ,useEffect} from 'react';
+import { useState ,useEffect } from "react";
 
-function EffectExaple()
+function EffectExample()
 {
   const[load, showload]=useState(0);
 
   useEffect(()=>
     {
       console.log("component mounted and updated")
-      return
-      {
+      return(
         console.log("component unmount")
-      }[load];
+     )[load];
     });
+    
             return(
                   <div> <h1>Hii this is load {load}</h1>
-                <button onClick=showload(()=load +1)> click me </button>
-              </div>
+                <button onClick={showload(() => load + 1)}> click me </button>
+             </div>
             );
 }
 export default EffectExample;
